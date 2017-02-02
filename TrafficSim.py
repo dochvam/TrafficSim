dd578de9deb74d42b3ed9c5fe3aa62891f993408
alabra26 <alexanderlabranche@gmail.com>
2017-02-01 20:24:28 -0800

    the reason it works is only because the car size is smaller than the speed limit

--- TrafficSim.py
+++ TrafficSim.py
@@ -11,7 +11,7 @@ def step(carArray, streetArray):
 win = GraphWin('test',500,500,False)
 
 streets = []
-streets.append(Street(Point(50, 100), Point(450, 200), 20, win))
+streets.append(Street(Point(50, 100), Point(450, 200), 10, win))
 
 cars = []
 # cars.append(Car(locOnStreet,speed,win,street,color))
@@ -19,15 +19,16 @@ cars.append(Car(60,10,win,streets[0],"red"))
 cars.append(Car(1,5,win,streets[0],"blue"))
 cars.append(Car(30,1,win,streets[0],"yellow"))
 
-print(streets[0].printCarList())
-streets[0].sortList()
-print(streets[0].printCarList())
+##print(streets[0].printCarList())
+##streets[0].sortList()
+##print(streets[0].printCarList())
+##
 
 
 while (cars[0].speed > 0):
 	step(cars, streets)
 	win.update()
-	time.sleep(0.2)
+	time.sleep(0.1)
 
 win.getMouse()
 win.close()
